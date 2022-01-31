a28653ffd626126c69f87e89919fe9a16c2732e4
usgnusmig <usgnusmig@gmail.com>
2022-01-31 23:45:15 +0900

    Add BE

--- BE/app.js
+++ BE/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const mysql = require("mysql");
+const dbconfig = require("./config/db");
+
+const connection = mysql.createConnection(dbconfig);
 
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.send("hi");
+  connection.query("Select * from Users", (error, rows) => {
+    if (error) throw error;
+    console.log(rows);
+    res.send(rows);
+  });
 });
 
 app.listen(5000, () => {
-  console.log("5000 port에서 서버 실행");
+  console.log("5000 포트에서 서버 시작");
 });
---
+++ BE/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: "127.0.0.1",
+  user: "MySQL User Name",
+  password: "MySQL Password",
+  database: "test_db",
+};
